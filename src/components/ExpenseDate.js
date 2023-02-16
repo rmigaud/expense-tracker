636b1c35e3e2f878296c60195522fd4f1e8bf8e8
Ryan Migaud <rmigaud@asu.edu>
2023-02-15 22:25:52 -0800

    extract ExpenseDate component

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,12 @@
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <h2 className="date">
+      {month} / {day} / {year}
+    </h2>
+  );
+};
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <h2 className="date">
-        {month} / {day} / {year}
-      </h2>
+      <ExpenseDate date={props.date} />
       <h1 className="expense">{props.expense}</h1>
       <p className="cost">{props.cost}</p>
     </div>
