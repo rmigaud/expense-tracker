2fea3f826358f7cf84cf28b9587beae9ce0f014e
Ryan Migaud <rmigaud@asu.edu>
2023-02-28 20:43:00 -0800

    tracks new expeses state from Expense Form

--- src/components/UI/ExpenseForm.js
+++ src/components/UI/ExpenseForm.js
@@ -1,24 +1,51 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 import Card from "./Card";
 
 const ExpenseForm = () => {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [cost, setCost] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  };
+
+  const costChangeHandler = (event) => {
+    setCost(event.target.value);
+  };
+
+  const submitExpenseHandler = (event) => {
+    console.log(title, date, cost);
+  };
+
   return (
     <form className="expense-form">
       <Card className="expense-input">
         <label for="date">date:</label>
-        <input id="date" />
+        <input id="date" type="date" onChange={dateChangeHandler} />
       </Card>
       <Card className="expense-input">
         <label for="title">title:</label>
-        <input id="title" />
+        <input id="title" onChange={titleChangeHandler} />
       </Card>
       <Card className="expense-input">
         <label for="cost">cost:</label>
-        <input id="cost" />
+        <input
+          id="cost"
+          type="number"
+          min="0.01"
+          step="0.01"
+          onChange={costChangeHandler}
+        />
       </Card>
       <div>
         <Card className="expense-input">
-          <button>Track Expense</button>
+          <button onClick={submitExpenseHandler}>Track Expense</button>
         </Card>
       </div>
     </form>
